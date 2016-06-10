9573dabcf4a6e056f6e19ec029346b05e2e5b5a7
Megan Strachan <m.e.strachan@gmail.com>
2016-06-10 11:12:45 +0100

    add total cash and add or remove cash methods

--- weekend_homework/start_point/pet_shop.rb
+++ weekend_homework/start_point/pet_shop.rb
@@ -1,6 +1,21 @@
 
-def pet_shop_name(pet_shop)
+# def pet_shop_name(pet_shop)
 
-  return pet_shop[:name]
+#   return pet_shop[:name]
 
+# end
+
+def total_cash(pet_shop)
+
+  return pet_shop[:admin] [:total_cash]
+
+end
+
+def add_or_remove_cash(pet_shop, number)
+
+  pet_shop[:admin] [:total_cash] = pet_shop[:admin] [:total_cash] + number
+
+end
+
+def
 end
--- weekend_homework/start_point/specs/pet_shop_spec.rb
+++ weekend_homework/start_point/specs/pet_shop_spec.rb
@@ -73,21 +73,21 @@ class TestPetShop < Minitest::Test
   end
 
   #Mandatory
-  def test_pet_shop_name
-    name = pet_shop_name(@pet_shop)
-    assert_equal(name, "Camelot of Pets")
-  end
+  # def test_pet_shop_name
+  #   name = pet_shop_name(@pet_shop)
+  #   assert_equal(name, "Camelot of Pets")
+  # end
 
   # def test_total_cash
   #   sum = total_cash(@pet_shop)
   #   assert_equal(sum, 1000)
   # end
 
-  # def test_add_or_remove_cash__add
-  #   add_or_remove_cash(@pet_shop,10)
-  #   cash = total_cash(@pet_shop)
-  #   assert_equal(cash, 1010)
-  # end
+  def test_add_or_remove_cash__add
+    add_or_remove_cash(@pet_shop,10)
+    cash = total_cash(@pet_shop)
+    assert_equal(cash, 1010)
+  end
 
   # def test_add_or_remove_cash__remove
   #   add_or_remove_cash(@pet_shop,-10)
