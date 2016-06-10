ff281a033f3a7159cfc1fb4a1b5e915fb1a9afe5
Megan Strachan <m.e.strachan@gmail.com>
2016-06-10 11:50:09 +0100

    add pets by breed method

--- weekend_homework/start_point/pet_shop.rb
+++ weekend_homework/start_point/pet_shop.rb
@@ -1,7 +1,6 @@
-
-# def pet_shop_name(pet_shop)
-#   return pet_shop[:name]
-# end
+def pet_shop_name(pet_shop)
+  return pet_shop[:name]
+end
 
 def total_cash(pet_shop)
   return pet_shop[:admin] [:total_cash]
@@ -24,5 +23,53 @@ def stock_count(pet_shop)
 end
 
 def pets_by_breed(pet_shop, breed)
-  return pet_shop[:pets].count if pet_shop[:pets] [:breed].include?(breed)
+
+  matched_pets = []
+
+  for pet in pet_shop[:pets]
+    matched_pets << pet if pet[:breed] == breed
+  end
+  return matched_pets
 end
+
+# def test_find_pet_by_name__returns_pet
+#   pet = find_pet_by_name(@pet_shop, "Arthur")
+#   assert_equal(pet[:name], "Arthur")
+# end
+
+# def find_pet_by_name(pet_shop, pet_name)
+
+
+
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
